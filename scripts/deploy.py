270585e085ac68c5326e8741ebed96a6608a2a3f
Domar95 <87876261+Domar95@users.noreply.github.com>
2022-04-09 23:47:16 +0200

    update

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,23 @@
-from brownie import accounts
+from brownie import accounts, config, Charity
+import time
 
 
 def deploy_simpleCharity():
-    # account = accounts[0]
-    account = accounts.load("30d_challenge-acc")
+    chain = "ganache"
+    if chain == "ganache":
+        account = accounts[0]
+    elif chain == "ropsten":
+        account = accounts.add(config["wallets"]["private_key"])
+
+    charity_contract = Charity.deploy({"from": account})
+    time.sleep(1)
+    charity_donate = charity_contract.donate(
+        "test donation", {"from": account, "amount": 100000}
+    )
+    time.sleep(1)
+    print(charity_donate)
+    get_balance = charity_contract.getBalance()
+    print(get_balance)
 
 
 def main():
